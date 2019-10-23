8d0cc8239b20e457c828438e18c37c30717077dc
ArvindSoma <arvindsoma@gmail.com>
2019-10-23 18:51:12 +0200

    Fixing errors

--- estimators/train_sparse_nocs.py
+++ estimators/train_sparse_nocs.py
@@ -36,7 +36,7 @@ def main(opt):
 
     coco_parent_dir = os.environ['COCO']
 
-    main_writer = namedtuple('main_writer', ('train', 'test'))
+    main_writer = namedtuple('main_writer', ('train', 'validate'))
     main_writer = main_writer(SummaryWriter(os.path.join(opt.log_dir, 'train')),
                               SummaryWriter(os.path.join(opt.log_dir, 'test')))
 
@@ -45,7 +45,7 @@ def main(opt):
     test_loader = SparsePointLoader(train=False, parent_dir=coco_parent_dir)
     test_loader = DataLoader(test_loader, batch_size=opt.batch_size, num_workers=2)
 
-    data_loader = namedtuple('data_loader', ('train', 'test'))
+    data_loader = namedtuple('data_loader', ('train', 'validate'))
     data_loader = data_loader(train_loader, test_loader)
     for epoch in range(opt.epochs):
         noc_trained.run(opt=opt, data_loader=data_loader, writer=main_writer, epoch=epoch)
