f3ed7bf3d904d123fc53b21740eb37799c388101
ArvindSoma <arvindsoma@gmail.com>
2019-12-04 15:33:25 +0100

    Adding visualization save loc

--- test/validate_sparse_nocs.py
+++ test/validate_sparse_nocs.py
@@ -52,10 +52,10 @@ def main(opt):
 
 
 if __name__ == "__main__":
-    opt = parse_args(['--log_dir=../data/logs/sparse_sampled_test_Res50UNet_Dropout_2Head_fixed_points_1',
+    opt = parse_args(['--log_dir=../data/logs/sparse_sampled_test_Res18UNet_3Decoder_Dropout_2Head_Coeff_100',
                       '--log_iter=200',
                       '--batch_size=8',
-                      '--checkpoint=../saves/sparse_sampled_train_Res50UNet_Dropout_2Head_fixed_points_1/save_330499.pth',
+                      '--checkpoint=../saves/sparse_sampled_train_Res18UNet_Dropout_2Head_Coeff_100/save_330499.pth',
                       '--num_heads=two',
                       '--model_type=res_unet',
                       '--backbone=res18'] + sys.argv[1:])
